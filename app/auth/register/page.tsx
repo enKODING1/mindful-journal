104bae9caffed41145fd01a7db6b9f0d3afb2dbb
Seongjin Kang <65354945+enKODING1@users.noreply.github.com>
2025-09-08 14:54:18 +0900

    feat: 회원가입 구현

---
+++ app/auth/register/page.tsx
@@ -0,0 +1,46 @@
+import {createClient }from '@/app/utils/supabase/server';
+
+export default function RegisterPage()
+{
+
+  const signUpNewUser = async (formdata: FormData) =>  {
+    'use server';
+    const email = formdata.get('email')?.toString();
+    const password = formdata.get('password')?.toString();
+    const supabaseClient = await createClient();
+
+    console.log(email, password);
+    if (!(email && password))
+    {
+      return ;
+    }
+
+    const {data, error} = await supabaseClient.auth.signUp({email: email, password: password });
+
+    if (error)
+    {
+      console.log(email, password)
+      console.error('회원가입 실패');
+      console.error(error);
+      console.log(data);
+    }else 
+    {
+      console.log("가입이 완료되었음 ");
+      console.log(data);
+    }
+  }
+
+  return (
+    <div>
+      <form action={signUpNewUser}>
+        <div>
+          <section>email</section>
+          <textarea name="email" ></textarea>
+          <section>password</section>
+          <textarea name="password"></textarea>
+        </div>
+        <button>가입</button>
+      </form>
+    </div> 
+  );
+}
