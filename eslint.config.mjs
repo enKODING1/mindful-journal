1cebe63b9d8d089f2a5fbc14051c9a7d7f34c731
Seongjin Kang <65354945+enKODING1@users.noreply.github.com>
2025-11-08 12:48:52 +0900

    feat: 스토리북 셋업

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook';
+
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
@@ -12,8 +15,16 @@ const compat = new FlatCompat({
 const eslintConfig = [
     ...compat.extends('next/core-web-vitals', 'next/typescript'),
     {
-        ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+        ignores: [
+            'node_modules/**',
+            '.next/**',
+            'out/**',
+            'build/**',
+            'next-env.d.ts',
+            'src/stories/**', // Storybook 예제 파일 제외
+        ],
     },
+    ...storybook.configs['flat/recommended'],
 ];
 
 export default eslintConfig;
